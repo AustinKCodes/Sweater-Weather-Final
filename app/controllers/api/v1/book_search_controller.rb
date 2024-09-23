c871d502160b733bdbb565612a3ed702f78a35fd
Austin K <austink@Austins-MacBook-Pro.local>
2024-09-23 11:16:55 -0400

    Route and Controller made

---
+++ app/controllers/api/v1/book_search_controller.rb
@@ -0,0 +1,5 @@
+class Api::V1::BookSearchcontroller < ApplicationController
+  def show
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get "forecast", to: "forecast#show"
+
+      get "/book-search", to: "book_search#show"
     end
   end
 end
